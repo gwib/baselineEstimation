50817f144e0deb774eb9381be64486b4791f077c
gwib <gwib@users.noreply.github.com>
2019-10-04 12:11:37 +0200

    f and A matrix

--- coordEstEquations.m
+++ coordEstEquations.m
@@ -1,7 +1,11 @@
-A = [1 0;
-    0 1;
-    0 1;
-    1 0;
-    0 -1;
-    1 -1;
-    1 0];
+A_transp = transpose(A);
+
+At_P_A = A_transp*P*A;
+At_P_f = A_transp*P*f;
+
+% estimated heights
+x_hat = inv(At_P_A) * At_P_f;
+
+
+% estimated corrections
+v_hat = A*x_hat- f;
