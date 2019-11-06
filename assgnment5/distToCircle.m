61fb4c56403fe3759de6b91ff6b47784682f20af
gwib <gwib@users.noreply.github.com>
2019-11-06 20:21:05 +0100

    getting started on assignment 5

---
+++ assgnment5/distToCircle.m
@@ -0,0 +1,8 @@
+function [outputArg1,outputArg2] = distToCircle(inputArg1,inputArg2)
+%DIRSTANCETOCIRCLE Summary of this function goes here
+%   in: circle params and point coordinates; out: distance of point to
+%   circle
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
---
+++ assgnment5/estimateCircle.m
@@ -0,0 +1,14 @@
+function [outputArg1,outputArg2] = estimateCircle(inputArg1,inputArg2)
+%ESTIMATECIRCLE estimates circle parameters x0, y0 and r based on three
+%input points
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+
+% estimate origin by taking the average of the three coordinates ...
+% for x and y, respectively
+
+% estimate origin radius by calculating the avg distance of the points to
+% the centre
+end
+
